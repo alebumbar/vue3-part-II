65991b6695a603c88a13eec4d8290c4cca7d9d00
Alexandra Bumbar(work) <alexandrabumbar@ip-192-168-1-160.ec2.internal>
2026-04-07 16:16:09 +0300

    using v-model

--- scripts.js
+++ scripts.js
@@ -1,24 +1,28 @@
 members = [
-    {
-        fname: 'John',
-        lname: 'Lennon',
-        instrument: 'Acoustic Guitar'
-    },
-    {
-        fname: 'George',
-        lname: 'Harrison',
-        instrument: 'Electric Guitar'
-    }
-]
+  {
+    fname: "John",
+    lname: "Lennon",
+    instrument: "Acoustic Guitar",
+  },
+  {
+    fname: "George",
+    lname: "Harrison",
+    instrument: "Electric Guitar",
+  },
+];
 
 const handlingForms = {
-
-    data() {
-        return {
-            members: window.members
-        }
+  data() {
+    return {
+      members: window.members,
+      newMember: {},
+    };
+  },
+  methods: {
+    addMember: function () {
+      this.members.push(this.newMember);
     },
-
+  },
 };
 
-Vue.createApp(handlingForms).mount('#app');
+Vue.createApp(handlingForms).mount("#app");
